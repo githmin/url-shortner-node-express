441808b78e14a0344095baef1d3e8673f38628fa
Githmin Jayawardhana <githmin21@gmail.com>
2022-11-10 21:47:49 +0530

    added mongodb connection

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 require('dotenv').config()
 
 const express = require('express')
+const { default: mongoose } = require('mongoose')
 const app = express()
+
+mongoose.connect(process.env.mongoDB)
+const db = mongoose.connection
+db.on('error', ()=>{
+    console.log(error)
+})
+
+db.on('open', ()=>{
+    console.log("Connected to db")
+})
+
+
+
 
 app.get('/', (req,res,next)=>{
     res.send("Connection success")
 })
+
+
 
 app.listen(process.env.port, ()=> {
     console.log(`Listening for connections on port ${process.env.port}`)
