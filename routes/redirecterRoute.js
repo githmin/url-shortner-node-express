a798ba2a0b6d50869b356ddeabd25e7e65ba97e8
Githmin Jayawardhana <githmin21@gmail.com>
2022-11-11 10:04:44 +0530

    added click incrementing to db

--- routes/redirecterRoute.js
+++ routes/redirecterRoute.js
@@ -4,6 +4,7 @@ const urlSchema = require('../models/URL')
 
 router.get('/:id', async (req,res,next)=>{
     const fullInfo = await urlSchema.find().where('short').equals(req.params.id)
+    await urlSchema.findOneAndUpdate({ _id:fullInfo[0]._id }, { $inc: { clicks : 1} })
     res.redirect(`https://${fullInfo[0].full}`)
 })
 
