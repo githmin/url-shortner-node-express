0a38917166dad8c851569f3e1a57a3829725813b
Githmin Jayawardhana <githmin21@gmail.com>
2022-11-11 12:40:11 +0530

    fixed sort by creation time

--- models/URL.js
+++ models/URL.js
@@ -4,7 +4,7 @@ const urlSchema = new mongoose.Schema({
     full : { type : String , required : true},
     short : { type : String , required : true},
     clicks : { type : Number , default : 0},
-    createdAt : { type: Number, default: (new Date()).getTime() }
+    createdAt : { type: Number, default: new Date() }
 })
 
 module.exports = mongoose.model("urlSchema", urlSchema)
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -4,7 +4,7 @@ const urlModel = require('../models/URL')
 
 
 router.get('/' , async (req,res,next)=>{
-    const urlList = await urlModel.find()
+    const urlList = await urlModel.find().sort ( { createdAt : -1 } )
     res.render('index', { urlList : urlList})
 })  
 
